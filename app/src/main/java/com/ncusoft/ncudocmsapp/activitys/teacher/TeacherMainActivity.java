9eaba565f4f0f2b2d7530735a7a7dbc6f8606dac
66498517 <51531662+66498517@users.noreply.github.com>
2019-06-16 15:17:28 +0800

    001

--- app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherMainActivity.java
+++ app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherMainActivity.java
@@ -1,6 +1,11 @@
 package com.ncusoft.ncudocmsapp.activitys.teacher;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ncusoft.ncudocmsapp.R;
 import com.ncusoft.ncudocmsapp.activitys.BaseActivity;
@@ -10,5 +15,41 @@ public class TeacherMainActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main);
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_base,menu);
+        return true;
+    }
+
+        @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_teacher_myclass:
+                Toast.makeText(this, "我的课程", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_teacher_selectclass:
+                Toast.makeText(this, "选择课程", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_teacher_addclass:
+               Toast.makeText(this, "添加课程", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_teacher_mymessage:
+                Toast.makeText(this, "个人信息", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_teacher_notice:
+                Toast.makeText(this, "信息通知", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_teacher_appraise:
+                Toast.makeText(this, "学生评价", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
 }
+
+
