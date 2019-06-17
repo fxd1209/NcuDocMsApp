fab6dd375cbd2436be78d5b37c0408abe43e64db
66498517 <51531662+66498517@users.noreply.github.com>
2019-06-17 18:31:27 +0800

    006

--- app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherNotice.java
+++ app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherNotice.java
@@ -1,14 +1,15 @@
 package com.ncusoft.ncudocmsapp.activitys.teacher;
 
 import android.os.Bundle;
+
 import com.ncusoft.ncudocmsapp.R;
 import com.ncusoft.ncudocmsapp.activitys.BaseActivity;
 
-public class AdminAddCourseActivity extends BaseActivity {
+public class TeacherNotice extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_addcourse);
-        initBaseToolbar("添加课程");
+        setContentView(R.layout.teacher_notice);
+        initBaseToolbar("信息通知");
     }
 }
