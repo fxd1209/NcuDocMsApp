4f2c5f194787e859c94157263942d5cd485cc0ab
fxd1209 <fxd1209@qq.com>
2019-06-17 21:54:04 +0800

    Update TeacherInfoActivity.java

--- app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherInfoActivity.java
+++ app/src/main/java/com/ncusoft/ncudocmsapp/activitys/teacher/TeacherInfoActivity.java
@@ -1,6 +1,9 @@
 package com.ncusoft.ncudocmsapp.activitys.teacher;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.ncusoft.ncudocmsapp.R;
@@ -17,6 +20,8 @@ public class TeacherInfoActivity extends BaseActivity {
     private EditText editEmail;
     private EditText editProTitle;
 
+    private Button btnInfoChange;
+
     TeacherServiceInterface teacherService;
 
     @Override
@@ -30,6 +35,9 @@ public class TeacherInfoActivity extends BaseActivity {
         editPhone=(EditText)findViewById(R.id.teacher_information_phonenumber);
         editEmail=(EditText)findViewById(R.id.teacher_information_email);
         editProTitle=(EditText)findViewById(R.id.teacher_information_protitle);
+
+        btnInfoChange=(Button)findViewById(R.id.btn_teacher_info_change);
+
         teacherService=new TeacherService();
         Teacher teacher=teacherService.getUserInfo();
         editName.setText(teacher.getName());
@@ -37,5 +45,20 @@ public class TeacherInfoActivity extends BaseActivity {
         editPhone.setText(teacher.getPhone());
         editEmail.setText(teacher.getEmail());
         editProTitle.setText(teacher.getProTitle());
+        btnInfoChange.setOnClickListener(onClickListener);
     }
+    private View.OnClickListener onClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_teacher_info_change:
+                    editName.setEnabled(true);
+                    editSex.setEnabled(true);
+                    editPhone.setEnabled(true);
+                    editEmail.setEnabled(true);
+                    editProTitle.setEnabled(true);
+                    break;
+            }
+        }
+    };
 }
