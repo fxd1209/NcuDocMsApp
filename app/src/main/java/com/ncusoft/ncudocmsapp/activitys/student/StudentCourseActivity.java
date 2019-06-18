36270788f87381e2e354b5f29c01aaf458f6da35
66498517 <51531662+66498517@users.noreply.github.com>
2019-06-18 22:19:04 +0800

    016

--- app/src/main/java/com/ncusoft/ncudocmsapp/activitys/student/StudentCourseActivity.java
+++ app/src/main/java/com/ncusoft/ncudocmsapp/activitys/student/StudentCourseActivity.java
@@ -1,16 +1,307 @@
 package com.ncusoft.ncudocmsapp.activitys.student;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
+import com.ncusoft.ncudocmsapp.ClientApplication;
 import com.ncusoft.ncudocmsapp.R;
 import com.ncusoft.ncudocmsapp.activitys.BaseActivity;
+import com.ncusoft.ncudocmsapp.activitys.course.TeacherCourseAdapter;
+import com.ncusoft.ncudocmsapp.activitys.teacher.TeacherEditTeaCouActivity;
+import com.ncusoft.ncudocmsapp.activitys.teacher.TeacherStuListActivity;
+import com.ncusoft.ncudocmsapp.pojo.Teacher;
+import com.ncusoft.ncudocmsapp.pojo.TeacherCourse;
+import com.ncusoft.ncudocmsapp.repository.course.CourseDao;
+import com.ncusoft.ncudocmsapp.repository.course.StudentCourseDao;
+import com.ncusoft.ncudocmsapp.repository.course.TeacherCourseDao;
+import com.ncusoft.ncudocmsapp.service.course.CourseService;
+import com.ncusoft.ncudocmsapp.service.course.CourseServiceInterface;
+import com.ncusoft.ncudocmsapp.service.user.TeacherService;
+import com.ncusoft.ncudocmsapp.service.user.TeacherServiceInterface;
+import com.ncusoft.ncudocmsapp.utils.ToastUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class StudentCourseActivity extends BaseActivity {
-    @Override
 
+    public static final String TAG="student.TCActivity";
+    private int currentSel; //当前点击的课程
+    GridView courseGirdView;
+    TeacherCourseAdapter tcAdapter;
+    List<TeacherCourse> list;
+    Map<Teacher,List<TeacherCourse>> tCourseListMap;//当前教师选课信息
+
+    TeacherServiceInterface teacherService=new TeacherService();
+    CourseServiceInterface courseService=new CourseService();
+
+    //TODO 测试用
+    CourseDao courseDao=CourseDao.getInstance();
+    TeacherCourseDao teacherCourseDao=TeacherCourseDao.getInstance();
+    StudentCourseDao studentCourseDao=StudentCourseDao.getInstance();
+
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_course);
+        setContentView(R.layout.activity_teacher_course);
         initBaseToolbar("我的课程");
+
+//        //测试用
+//        ContentValues cvCourse1=new Course.CourseBuilder()
+//                .id("c789")
+//                .name("高等数学")
+//                .credit("3")
+//                .build().toContentValues();
+//        ContentValues cvCourse2=new Course.CourseBuilder()
+//                .id("c129")
+//                .name("大学语文")
+//                .credit("2")
+//                .build().toContentValues();
+//        courseDao.insert(cvCourse1);
+//        courseDao.insert(cvCourse2);
+//
+//        ContentValues cvTc1=new TeacherCourse.TeacherCourseBuilder()
+//                .id(null)
+//                .teacherId("18748980084")
+//                .courseId("c123")
+//                .term("2018-2019-2")
+//                .classCount("2")
+//                .build().toContentValues();
+//        ContentValues cvTc2=new TeacherCourse.TeacherCourseBuilder()
+//                .id(null)
+//                .teacherId("18748980084")
+//                .courseId("c456")
+//                .term("2018-2019-1")
+//                .classCount("1")
+//                .build().toContentValues();
+//        ContentValues cvTc3=new TeacherCourse.TeacherCourseBuilder()
+//                .id(null)
+//                .teacherId("18798891209")
+//                .courseId("c123")
+//                .term("2018-2020-1")
+//                .classCount("5")
+//                .build().toContentValues();
+//
+//        teacherCourseDao.insert(cvTc1);
+//        teacherCourseDao.insert(cvTc2);
+//        teacherCourseDao.insert(cvTc3);
+//        ContentValues cvSc1=new StudentCourse.StudentCourseBuilder()
+//            .id(null)
+//            .studentId("18780451091")
+//            .courseId("c123")
+//            .term("2018-2019-2")
+//            .classCount("2")
+//            .build().toContentValues();
+//        ContentValues cvSc2=new StudentCourse.StudentCourseBuilder()
+//            .id(null)
+//            .studentId("18780451091")
+//            .courseId("c456")
+//            .term("2018-2019-1")
+//            .classCount("1")
+//            .build().toContentValues();
+//        ContentValues cvSc4=new StudentCourse.StudentCourseBuilder()
+//                .id(null)
+//                .studentId("18798891209")
+//                .courseId("c456")
+//                .term("2018-2019-1")
+//                .classCount("1")
+//                .build().toContentValues();
+//        ContentValues cvSc3=new StudentCourse.StudentCourseBuilder()
+//            .id(null)
+//            .studentId("18798891209")
+//            .courseId("c123")
+//            .term("2018-2020-1")
+//            .classCount("5")
+//            .build().toContentValues();
+//        studentCourseDao.insert(cvSc1);
+//        studentCourseDao.insert(cvSc2);
+//        studentCourseDao.insert(cvSc3);
+//        studentCourseDao.insert(cvSc4);
+
+        courseGirdView = (GridView) findViewById(R.id.teacher_course_grid_view);
+
+        //查询当前登录者选课信息
+
+        tCourseListMap=teacherService.getTeaCourseByTeacherId(
+                ClientApplication.getInstance().getCurrentLoginUser().getId());
+        Set<Teacher> teacherSet=tCourseListMap.keySet();
+        for (Teacher teacher : teacherSet){
+            if (teacher==null) break;
+            list=tCourseListMap.get(teacher);
+        }
+        tcAdapter=new TeacherCourseAdapter(StudentCourseActivity.this,list);
+        courseGirdView.setAdapter(tcAdapter);
+        registerForContextMenu(courseGirdView);
+        courseGirdView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                currentSel=position; //记录点击的课程位置
+                courseGirdView.showContextMenu();
+            }
+        });
+    } //end onCreate
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //重新从数据库读出数据
+        Map<Teacher,List<TeacherCourse>> tempMap=null;
+        List<TeacherCourse> tempList=null;
+        tempMap=teacherService.getTeaCourseByTeacherId(
+                ClientApplication.getInstance().getCurrentLoginUser().getId());
+        Set<Teacher> teacherSet=tempMap.keySet();
+        for (Teacher teacher : teacherSet){
+            if (teacher==null) break;
+            tempList=tempMap.get(teacher);
+        }
+
+        //如果读出的数据和原来的list大小不一致，说明数据改变了
+        if (!(tempList!=null && tempList.size()==list.size())){
+            list=tempList;
+            tcAdapter.setCourseList(list);
+            tcAdapter.refreshDataSet();
+        }
+
     }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        menu.add(0,0,0,"查看学生");
+        menu.add(0,1,1,"添加学生");
+        menu.add(0,2,2,"编辑课程");
+        menu.add(0,3,3,"删除课程");
+        super.onCreateContextMenu(menu,v,menuInfo);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case 0: //点击查看学生
+                Intent intent=new Intent();
+                Bundle bundle =new Bundle();
+                intent.setClass(StudentCourseActivity.this,TeacherStuListActivity.class);
+                bundle.putSerializable("teacherCourse",list.get(currentSel));
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case 1: //点击添加学生
+                break;
+            case 2: //点击编辑课程
+                Intent intent1=new Intent();
+                Bundle bundle1 =new Bundle();
+                intent1.setClass(StudentCourseActivity.this,TeacherEditTeaCouActivity.class);
+                bundle1.putSerializable("teacherCourse",list.get(currentSel));
+                intent1.putExtras(bundle1);
+                startActivity(intent1);
+                break;
+            case 3: //点击删除课程
+                //可以设置三个按钮
+                AlertDialog dlg
+                        =new AlertDialog.Builder(StudentCourseActivity.this)
+                        .setIcon(R.drawable.icon_doubt)
+                        .setTitle("警告")
+                        .setMessage("确定删除吗？")
+                        .setPositiveButton("确定删除", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                TeacherCourse tcList=list.get(currentSel);
+                                if (0==courseService.deleteTeacherCourse(tcList)){
+                                    ToastUtil.initToast(StudentCourseActivity.this,
+                                            ToastUtil.ToastType.FAIL,"删除失败!",
+                                            Toast.LENGTH_LONG,new Point(0,0)).show();
+                                }else{
+                                    ToastUtil.initToast(StudentCourseActivity.this,
+                                            ToastUtil.ToastType.SUCCESS,"删除成功!",
+                                            Toast.LENGTH_LONG,new Point(0,0)).show();
+                                    //移除数据
+                                    list.remove(currentSel);
+                                    tcAdapter.remove(currentSel);
+                                    tcAdapter.refreshDataSet();
+
+                                }
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                System.out.println("c");
+                            }
+                        }).setNeutralButton("算了", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                System.out.println("a");
+                            }
+                        }).show();
+
+                break;
+            default:break;
+        }
+        return  super.onContextItemSelected(item);
+    }
+
 }
+
+
+/***
+ * 设置action bar ，需要主题样式里面有actionbar
+ */
+//  actionBar=getActionBar();
+//          actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//          actionBar.setCustomView(R.layout.toolbal_base);//自定义ActionBar布局
+//          actionBar.getCustomView().setOnClickListener(new View.OnClickListener() {//监听事件
+//@Override
+//public void onClick(View arg0) {
+//        switch (arg0.getId()) {
+////                    case R.id.back:
+////                        showToast(R.string.finish);
+////                        finish();
+////                        break;
+//default:
+//        break;
+//        }
+//        }
+//        });
+/*******************
+ *end
+ *****************/
+
+
+
+
+
+//    //toolbar菜单栏
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_toolbar_base,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.menu_1:
+//                Toast.makeText(this, "我是第一个", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.menu_2:
+//                Toast.makeText(this, "我是第二个", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.menu_3:
+//                Toast.makeText(this, "我是第三个", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.menu_4:
+//                Toast.makeText(this, "我是第四个", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//        return true;
+//    }
+
+
--- app/src/main/java/com/ncusoft/ncudocmsapp/activitys/student/StudentMainActivity.java
+++ app/src/main/java/com/ncusoft/ncudocmsapp/activitys/student/StudentMainActivity.java
@@ -14,6 +14,7 @@ import com.ncusoft.ncudocmsapp.activitys.teacher.AdminAddCourseActivity;
 import com.ncusoft.ncudocmsapp.activitys.teacher.TeacherCourseActivity;
 import com.ncusoft.ncudocmsapp.activitys.teacher.TeacherInfoActivity;
 import com.ncusoft.ncudocmsapp.activitys.teacher.TeacherSelectCouActivity;
+import com.ncusoft.ncudocmsapp.pojo.Student;
 
 public class StudentMainActivity extends BaseActivity {
 
@@ -25,67 +26,67 @@ public class StudentMainActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_main);
+        setContentView(R.layout.activity_student_main);
         initBaseToolbar("主菜单");
         InitBtns(); //初始化按钮，并绑定监听事件
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar_base,menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
     public void InitBtns(){
-        btnCourseList=findViewById(R.id.btn_teacher_courseList);
-        btnSelectCourse=findViewById(R.id.btn_teacher_selectCourse);
-        btnAddCourse=findViewById(R.id.btn_teacher_addCourse);
-        btnPersonInfo=findViewById(R.id.btn_teacher_info);
-        btnCourseList.setOnClickListener(new TeacherBtnListener());
-        btnSelectCourse.setOnClickListener(new TeacherBtnListener());
-        btnAddCourse.setOnClickListener(new TeacherBtnListener());
-        btnPersonInfo.setOnClickListener(new TeacherBtnListener());
+        btnCourseList=findViewById(R.id.btn_student_courseList);
+        btnSelectCourse=findViewById(R.id.btn_student_selectCourse);
+        btnAddCourse=findViewById(R.id.btn_student_addCourse);
+//        btnPersonInfo=findViewById(R.id.btn_stu_info);
+        btnCourseList.setOnClickListener(new StudentBtnListener());
+        btnSelectCourse.setOnClickListener(new StudentBtnListener());
+        btnAddCourse.setOnClickListener(new StudentBtnListener());
+//        btnPersonInfo.setOnClickListener(new TeacherBtnListener());
     }
         @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.menu_teacher_myclass:
+            case R.id.menu_student_myclass:
                 Toast.makeText(this, "我的课程", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_teacher_selectclass:
+            case R.id.menu_student_selectclass:
                 Toast.makeText(this, "选择课程", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_teacher_addclass:
+            case R.id.menu_student_addclass:
                Toast.makeText(this, "添加课程", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_teacher_mymessage:
+            case R.id.menu_student_mymessage:
                 Toast.makeText(this, "个人信息", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_teacher_notice:
+            case R.id.menu_student_notice:
                 Toast.makeText(this, "信息通知", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.menu_teacher_appraise:
+            case R.id.menu_student_appraise:
                 Toast.makeText(this, "学生评价", Toast.LENGTH_SHORT).show();
                 break;
         }
         return true;
     }
 
-    class TeacherBtnListener implements View.OnClickListener{
+    class StudentBtnListener implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.btn_teacher_courseList:
-                    startActivity(new Intent().setClass(StudentMainActivity.this, TeacherCourseActivity.class));
+                case R.id.btn_student_courseList:
+                    startActivity(new Intent().setClass(StudentMainActivity.this, StudentCourseActivity.class));
                     break;
-                case R.id.btn_teacher_selectCourse:
-                    startActivity(new Intent().setClass(StudentMainActivity.this, TeacherSelectCouActivity.class));
+                case R.id.btn_student_selectCourse:
+                    startActivity(new Intent().setClass(StudentMainActivity.this, StudentSelectCouActivity.class));
                     break;
-                case R.id.btn_teacher_addCourse:
+                case R.id.btn_student_addCourse:
                     startActivity(new Intent().setClass(StudentMainActivity.this, AdminAddCourseActivity.class));
                     break;
-                case R.id.btn_teacher_info:
+                case R.id.btn_student_info:
                     startActivity(new Intent().setClass(StudentMainActivity.this, TeacherInfoActivity.class));
                     break;
                     default:
